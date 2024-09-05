26d91f320ea5c94710f1a451f2dc2123b1d2d732
Lu, Kuang-Chen <kuang-chen_lu@brown.edu>
2024-09-05 12:05:50 -0400

    tested

--- test/Main.mjs
+++ test/Main.mjs
@@ -4,12 +4,13 @@ import * as Core__List from "@rescript/core/src/Core__List.mjs";
 import * as SExpression from "../src/SExpression.mjs";
 import * as Caml_js_exceptions from "rescript/lib/es6/caml_js_exceptions.js";
 
-function test_parse(str, wanted_result) {
+function test_parse(ignoreLangLineOpt, str, wanted_result) {
+  var ignoreLangLine = ignoreLangLineOpt !== undefined ? ignoreLangLineOpt : false;
   var result;
   var exit = 0;
   var elms;
   try {
-    elms = SExpression.SExpr.fromString(undefined, str);
+    elms = SExpression.SExpr.fromString(ignoreLangLine, str);
     exit = 1;
   }
   catch (raw_err){
@@ -37,41 +38,45 @@ function test_parse(str, wanted_result) {
   
 }
 
-test_parse("(", "Error: reached the end of the file while processing a list.");
+test_parse(undefined, "(", "Error: reached the end of the file while processing a list.");
 
-test_parse("\"", "Error: reached the end of the file while processing a string.");
+test_parse(undefined, "\"", "Error: reached the end of the file while processing a string.");
 
-test_parse("(]", "Error: found a closing square bracket while processing a list started with a round bracket.");
+test_parse(undefined, "(]", "Error: found a closing square bracket while processing a list started with a round bracket.");
 
-test_parse("[)", "Error: found a closing round bracket while processing a list started with a square bracket.");
+test_parse(undefined, "[)", "Error: found a closing round bracket while processing a list started with a square bracket.");
 
-test_parse("()", "()");
+test_parse(undefined, "()", "()");
 
-test_parse("[]", "[]");
+test_parse(undefined, "[]", "[]");
 
-test_parse(")", "Error: found an extra closing round bracket at 1:1.");
+test_parse(undefined, ")", "Error: found an extra closing round bracket at 1:1.");
 
-test_parse("]", "Error: found an extra closing square bracket at 1:1.");
+test_parse(undefined, "]", "Error: found an extra closing square bracket at 1:1.");
 
-test_parse("#t", "#t");
+test_parse(undefined, "#t", "#t");
 
-test_parse("#f", "#f");
+test_parse(undefined, "#f", "#f");
 
-test_parse("42", "42");
+test_parse(undefined, "42", "42");
 
-test_parse("\"foo\" \"bar\"", "\"foo\" \"bar\"");
+test_parse(undefined, "\"foo\" \"bar\"", "\"foo\" \"bar\"");
 
-test_parse("((a) () #t 42)", "((a) () #t 42)");
+test_parse(undefined, "((a) () #t 42)", "((a) () #t 42)");
 
-test_parse("\"\\n\"", "\"\n\"");
+test_parse(undefined, "\"\\n\"", "\"\n\"");
 
-test_parse("\"\\t\"", "\"\t\"");
+test_parse(undefined, "\"\\t\"", "\"\t\"");
 
-test_parse("\"\\?\"", "Error: found an unexpected escape sequence (\\?).");
+test_parse(undefined, "\"\\?\"", "Error: found an unexpected escape sequence (\\?).");
 
-test_parse("#;(ignore this s-expression) 2 3", "2 3");
+test_parse(undefined, "#;(ignore this s-expression) 2 3", "2 3");
 
-test_parse("\n;; ignroe this line\n2\n3\n", "2 3");
+test_parse(undefined, "\n;; ignroe this line\n2\n3\n", "2 3");
+
+test_parse(true, "#lang\n2 3", "2 3");
+
+test_parse(true, "\n\n\n\n#lang foo\n\n2\n3\n", "2 3");
 
 export {
   test_parse ,
